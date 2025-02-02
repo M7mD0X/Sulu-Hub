b0e6fe5e316f622c2f23bf4acf1a58592e46d4ea
M7mD0X <85457690+M7mD0X@users.noreply.github.com>
2025-02-02 05:12:53 +0300

    Update Sulu.lua

--- Sulu.lua
+++ Sulu.lua
@@ -1,16 +1,51 @@
 if game.PlaceId == 17625359962 then
 
-  -- Boot Ui Library
+  -- -- Boot Ui Library
   local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+  -- -- Create Window
+  local Window = Rayfield:CreateWindow({
+   Name = "Sulu Hub",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Rivals",
+   LoadingSubtitle = "by Cero",
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = "Sulu Hub", -- Create a custom folder for your hub/game
+      FileName = "Sulu Hub"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+  
   -- -- Create Tabs
   local MainTab = Window:CreateTab("Main", nil)
   local EspTab = Window:CreateTab("Esp", nil)
 
   -- -- Main Tab Scripts
   -- Aimbot Button
-  local Button = Tab:CreateButton({
-   Name = "Button Example",
+  local Button = MainTab:CreateButton({
+   Name = "AimBot",
    Callback = function()
         local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
